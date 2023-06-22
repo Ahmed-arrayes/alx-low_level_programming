8d0d1739883d7309a3aa814f25899b882278fa9b
Ahmed-arrayes <abdelhamedahmed45499@gmail.com>
2023-06-21 19:15:40 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include "main.h"
-
-/**
- * main - entry point of my code .
- *
- * Return: Always 0 (Success)
- */
+#include <main.h>
 int main(void)
-
-	int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
+  return (0);
 }
-
