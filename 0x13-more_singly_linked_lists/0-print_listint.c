bdae6dbcd912d0f45e4d7e9351481fbc5ca17c12
Ahmed-arrayes <abdelhamedahmed45499@gmail.com>
2023-08-01 10:41:11 -0700

    :f:

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints a linked lists
+ * print_listint - prints all elements of a linked list
  * @h: linked list of type listint_t to print 
  *
  * Return: number of nodes.
