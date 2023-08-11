3d7a28f4e36514f51f44adefc1648ae33513855b
Ahmed-arrayes <abdelhamedahmed45499@gmail.com>
2023-08-11 11:06:58 -0700

    :g:

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,33 @@
 #include "main.h"
 
 /**
-*read textfile function that reads a
-*text file and prints it to the POSIX standard output.
-*@filename: filename
-*@letters: number of letters to print
-*Return: Number of letters || e
-*/
-ssize_t read textfile(const char *filename, size_t letters)
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: number of letters printed.
+ *
+ * Return: number of letters printed. If it fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd;
-    ssize_t read, write;
-    char *buffer;
-    if (!filename)
-    (
-    }
-    return (0);
-    /"open file"/
-    fd = open(filename, O_RDONLY);
-        if (fd == -1)
-        return (0);
+	FILE *file;
+	char *buffer;
+	ssize_t nrd, nwr;
 
+	if (!filename)
+		return (0);
 
-    buf = malloc(sizeof(char) letters);
-    if (buffer == NULL)
-        return (0);
+	file = fopen(filename, "r");
+	if (!file)
+		return (0);
 
-    _read = read(fd, buffer, letters);
-    if (read == -1)
-    (
-        free(buffer);
-        close(fd);
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
 
-    _write = write(STDOUT FILENO, buffer, read);
-    if (write == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-    close(fd);
-    return (read);
+	nrd = fread(buffer, sizeof(char), letters, file	nwr = fwrite(buffer, sizeof(char), nrd, stdout);
+
+	fclose(file);
+	free(buffer);
+
+	return nwr;
 }
