870cf2a048ce8122094be650a4ac3ef4e4771057
Ahmed-arrayes <abdelhamedahmed45499@gmail.com>
2023-08-11 09:51:32 -0700

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,30 +7,39 @@
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t _read, _write;
-	char *buffer;
+        int fd;
+    ssize_t read, write;
+    char *buf;
+    if (Ifilename)
+    (
+    }
+    return (0);
+    /"open file"/
+    fd = open(filename, O_RDONLY);
+        if (fd == -1)
+        return (0);
 
-	if (!filename)
-		return (0);
 
-	fd = open(filename, O_RDONLY);
+    buf = malloc(sizeof(char) letters);
+    if (buf == NULL)
+        return (0);
 
-	if (fd == -1)
-		return (0);
+    _read = read(fd, buf, letters);
+    if (read == -1)
+    (
+        free(buf);
+        close(fd);
+        return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
-		return (0);
-
-	_read = read(fd, buffer, letters);
-	_write = write(STDOUT_FILENO, buffer, _read);
-
-	close(fd);
-
-	free(buffer);
-
-	return (read);
+    _write = write(STDOUT FILENO, buf, read);
+    if (write == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+    close(fd);
+    return (read);
 }
